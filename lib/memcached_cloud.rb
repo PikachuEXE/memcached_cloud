465f009e9c684f3fdb31c0097ea98a0124c1649e
PikachuEXE <pikachuexe@gmail.com>
2014-03-27 11:42:34 +0800

    ! Put the hash inline to try to fix error in MRI 1.8

--- lib/memcached_cloud.rb
+++ lib/memcached_cloud.rb
@@ -1,16 +1,14 @@
 require 'memcached_cloud/version'
 
 module MemcachedCloud
-  MEMCACHEDCLOUD_TO_MEMCACHE_ENV_VAR_MAPPINGS ||= {
-    "MEMCACHEDCLOUD_SERVERS" => "MEMCACHE_SERVERS",
-    "MEMCACHEDCLOUD_USERNAME" => "MEMCACHE_USERNAME",
-    "MEMCACHEDCLOUD_PASSWORD" => "MEMCACHE_PASSWORD",
-  }.freeze
-
   extend self
 
   def setup
-    MEMCACHEDCLOUD_TO_MEMCACHE_ENV_VAR_MAPPINGS.each do |key, value|
+    {
+      "MEMCACHEDCLOUD_SERVERS" => "MEMCACHE_SERVERS",
+      "MEMCACHEDCLOUD_USERNAME" => "MEMCACHE_USERNAME",
+      "MEMCACHEDCLOUD_PASSWORD" => "MEMCACHE_PASSWORD",
+    }.each do |key, value|
       ENV[value] = ENV[key] if ENV[key]
     end
   end
